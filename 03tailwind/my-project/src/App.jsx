84e48cbc19471c021d2fb77a3098f95cfbfd9a31
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-09-29 01:59:10 -0400

    tailwind

---
+++ 03tailwind/my-project/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div className="text-blue-500 m-0 p-0 bg-black">I love Coding...</div>
+    <p className="bg-slate-500 p-20 rounded-2xl m-2 text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi sequi aspernatur quos.</p>
+    
+    </> 
+  )
+}
